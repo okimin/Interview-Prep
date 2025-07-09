d4d169a387cf4f999992437265957ad27a4a072f
Daniel Gaston <danielgaston6@gmail.com>
2025-07-09 12:24:52 -0400

    Interleaving Strings

--- DSA/NeetCode 150/97.Interleaving-Strings/Solution.java
+++ DSA/NeetCode 150/97.Interleaving-Strings/Solution.java
@@ -1,5 +1,29 @@
-public class Solution {
+
+class Solution {
+    boolean yesRec(int i, int j, String s1, String s2, String s3, Boolean[][] dp) {
+        if (i + j == s3.length())
+            return true;
+        
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        boolean res = false;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)) {
+            res |= yesRec(i + 1, j, s1, s2, s3, dp);
+        }
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(i + j)) {
+            res |= yesRec(i, j + 1, s1, s2, s3, dp);
+        }
+
+        return dp[i][j] = res;
+    }
     public boolean isInterleave(String s1, String s2, String s3) {
-        if()
+        int n = s1.length(), m = s2.length();
+        if (n + m != s3.length())
+            return false;
+        
+        Boolean[][] dp = new Boolean[n + 1][m + 1];
+
+        return yesRec(0, 0, s1, s2, s3, dp);
     }
 }
